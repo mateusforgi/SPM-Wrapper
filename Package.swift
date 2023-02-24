75dc8d61144f8280bf0846dc0d87fa396e98f2a3
Mateus Forgiarini da Silva <mateusforgi@gmail.com>
2023-02-24 11:43:56 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,32 +1,24 @@
 // swift-tools-version:5.5
-//
-// The `swift-tools-version` declares the minimum version of Swift required to
-// build this package. Do not remove it.
 
 import PackageDescription
 
 let package = Package(
-  name: "SPM-Wrapper",
-  platforms: [
-    .macOS(.v10_14),
-    .iOS(.v9),
-    .watchOS(.v4),
-    .tvOS(.v11)
-  ],
-  products: [
-    .library(name: "NordicDFUDynamic", type: .dynamic, targets: ["NordicDFUDynamic"])
-  ],
-  dependencies: [
-    .package(url: "https://github.com/NordicSemiconductor/IOS-DFU-Library", from: "4.12.0"),
-  ],
-  targets: [
-    .target(
-        name: "NordicDFUDynamic",
-        dependencies: [
-            .product(name: "NordicDFU", package: "IOS-DFU-Library")
-        ]
-    )
-  ],
-  swiftLanguageVersions: [.v5]
+    name: "NordicDFUDynamic",
+    platforms: [.iOS(.v14)],
+    products: [
+        .library(
+            name: "NordicDFUDynamic",
+            type: .dynamic,
+            targets: ["NordicDFUDynamic"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/NordicSemiconductor/IOS-DFU-Library", .exact("4.12.0")),
+    ],
+    targets: [
+        .target(
+            name: "NordicDFUDynamic",
+            dependencies: [.product(name: "NordicDFU", package: "IOS-DFU-Library")]
+        ),
+    ]
 )
-
